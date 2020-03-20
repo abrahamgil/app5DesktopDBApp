b6995990f4cc156d07a0540780656febcfbd2ca3
Abraham <abrahamgil2@gmail.com>
2020-03-20 02:27:26 -0500

    added some documentation about architecture and plan

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
-print("New app")
+"""
+A program that stores the following book information:
+Title, Author
+Year, ISBN
+
+The user wwill have the following functionality:
+View all records
+Search an entry
+Add entry
+Update entry
+Delete
+Close
+"""
