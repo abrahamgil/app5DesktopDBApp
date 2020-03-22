ab301474b348972b4b5a27d0fd6ce51f4e62ab94
Abraham <abrahamgil2@gmail.com>
2020-03-22 03:59:06 -0500

    Created delete and update functions

--- backend.py
+++ backend.py
@@ -22,7 +22,32 @@ def view():
     conn.close()
     return rows
 
+def search(title="", author="", year="", isbn=""):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book where id=?",(id,))
+    conn.commit()
+    conn.close()
+
+def update(id,title, author, year, isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
+    conn.commit()
+    conn.close()
+
+
 
 connect()
-insert("The sea", "John Tablet", 1918, 913123132)
+#insert("The Sun","John Smith",1920,8192304712)
+update(4,"The moon", "John Smooth", 1917, 9999999)
 print(view())
+print(search(author="John Smith"))
